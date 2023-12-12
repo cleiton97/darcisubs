21ac8ee6f1a663689832a3570a2f8d9b591f55fd
cleiton97 <146895519+cleiton97@users.noreply.github.com>
2023-12-12 21:21:38 +0000

    Substitutiva Darci

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className='text-center'>Minha Avaliação Substitutiva</h1>
+      <h1 className='text-center'>Minha Avaliação Substitutiva Darci</h1>
       
     </div>
   )
